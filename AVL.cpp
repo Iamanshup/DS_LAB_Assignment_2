62f342813ca53ddca1b223c2a303c2d6458f1359
iamanshup <raj.anshu66@gmail.com>
2021-08-31 21:43:15 +0530

    update AVL_Insert() function

--- AVL.cpp
+++ AVL.cpp
@@ -32,8 +32,8 @@ class AVL_Tree
 private:
 	AVL_Node *root;
 	AVL_Node *link(int a, AVL_Node *node);
-	void singleRotation(AVL_Node *p, AVL_Node *r, AVL_Node *s, int a);
-	void doubleRotation(AVL_Node *p, AVL_Node *r, AVL_Node *s, int a);
+	void singleRotation(AVL_Node *&p, AVL_Node *&r, AVL_Node *&s, int a);
+	void doubleRotation(AVL_Node *&p, AVL_Node *&r, AVL_Node *&s, int a);
 	void AVL_PrintHelper(const AVL_Node *node, ofstream &fout);
 
 public:
@@ -68,6 +68,12 @@ void AVL_Tree::AVL_Insert(int k)
 
 	AVL_Node *avl_node = new AVL_Node(k);
 
+	if (!p)
+	{
+		root->RChild = avl_node;
+		return;
+	}
+
 	while (true)
 	{
 		if (k < p->key)
@@ -95,14 +101,14 @@ void AVL_Tree::AVL_Insert(int k)
 			throw "Element already exists.";
 		}
 
-		t = p;
-		s = q;
+		if (q->bf != 0)
+		{
+			t = p;
+			s = q;
+		}
+
 		p = q;
 	}
-
-	// q->key = k;
-	// q->LChild = q->RChild = nullptr;
-	// q->bf = 0;
 
 	int a = 1;
 	if (k < s->key)
@@ -145,47 +151,36 @@ void AVL_Tree::AVL_Insert(int k)
 	}
 
 	if (s == t->RChild)
-	{
 		t->RChild = p;
-	}
 	else
-	{
 		t->LChild = p;
-	}
 }
 
-void AVL_Tree::singleRotation(AVL_Node *p, AVL_Node *r, AVL_Node *s, int a)
+void AVL_Tree::singleRotation(AVL_Node *&p, AVL_Node *&r, AVL_Node *&s, int a)
 {
 	p = r;
 	if (a == 1)
-	{
-		s->LChild = r->RChild;
-		r->RChild = s;
-		s->bf = r->bf = 0;
-	}
-	else
 	{
 		s->RChild = r->LChild;
 		r->LChild = s;
-		s->bf = r->bf = 0;
 	}
+	else
+	{
+		s->LChild = r->RChild;
+		r->RChild = s;
+	}
+	s->bf = r->bf = 0;
 }
 
-void AVL_Tree::doubleRotation(AVL_Node *p, AVL_Node *r, AVL_Node *s, int a)
+void AVL_Tree::doubleRotation(AVL_Node *&p, AVL_Node *&r, AVL_Node *&s, int a)
 {
-	p = r;
-	if (a == 1)
+	if (a == -1)
 	{
 		p = r->RChild;
 		r->RChild = p->LChild;
 		p->LChild = r;
 		s->LChild = p->RChild;
 		p->RChild = s;
-		s->bf = p->bf == 0 ? 0 : p->bf == 1 ? -1
-											: 0;
-		r->bf = p->bf == 0 ? 0 : p->bf == 1 ? 0
-											: 1;
-		p->bf = 0;
 	}
 	else
 	{
@@ -194,12 +189,24 @@ void AVL_Tree::doubleRotation(AVL_Node *p, AVL_Node *r, AVL_Node *s, int a)
 		p->RChild = r;
 		s->RChild = p->LChild;
 		p->LChild = s;
-		s->bf = p->bf == 0 ? 0 : p->bf == 1 ? 0
-											: 1;
-		r->bf = p->bf == 0 ? 0 : p->bf == 1 ? -1
-											: 0;
-		p->bf = 0;
 	}
+
+	if (p->bf == a)
+	{
+		s->bf = -a;
+		r->bf = 0;
+	}
+	else if (p->bf == 0)
+	{
+		s->bf = 0;
+		r->bf = 0;
+	}
+	else if (p->bf == -a)
+	{
+		s->bf = 0;
+		r->bf = a;
+	}
+	p->bf = 0;
 }
 
 // TODO : use filename in system call
@@ -207,31 +214,37 @@ void AVL_Tree::AVL_Print(const char *filename)
 {
 	ofstream fout;
 
-	fout.open(filename);
+	fout.open("AVL.dot");
 	fout << "digraph g {\n";
 	AVL_PrintHelper(root->RChild, fout);
 	fout << "}";
 	fout.close();
 	// string command = "dot -Tpng " + filename + " -o AVL.png";
-	system("dot -Tpng AVL.gv -o AVL.png");
+	system("dot -Tpng AVL.dot -o AVL.png");
 }
 
 void AVL_Tree::AVL_PrintHelper(const AVL_Node *node, ofstream &fout)
 {
 	if (!node)
 		return;
-
-	if (node == root) // add the label and root in the dot file
+	cout << node->key << endl;
+	if (node == root->RChild) // add the label and root in the dot file
 	{
 		fout << "label = \" rooted at " << node->key << " \";\n";
 		fout << node->key << " [root = true]\n";
 	}
 
-	fout << node->key << " -> " << node->LChild->key << "\n";
-	fout << node->key << " -> " << node->RChild->key << "\n";
+	if (node->LChild)
+	{
+		fout << node->key << " -> " << node->LChild->key << "\n";
+		AVL_PrintHelper(node->LChild, fout);
+	}
 
-	AVL_PrintHelper(node->LChild, fout);
-	AVL_PrintHelper(node->RChild, fout);
+	if (node->RChild)
+	{
+		fout << node->key << " -> " << node->RChild->key << "\n";
+		AVL_PrintHelper(node->RChild, fout);
+	}
 }
 
 int main()
@@ -243,6 +256,6 @@ int main()
 		tree->AVL_Insert(i);
 	}
 
-	char filename[] = "AVL.gv";
+	char filename[] = "AVL.dot";
 	tree->AVL_Print(filename);
 }
